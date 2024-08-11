84303d9637d08cd8fddbe5c2f3425ea9b48dc754
baileysnyder <baileysnyderm@yahoo.com>
2024-08-10 20:38:27 -0500

    Make height smaller on mobile. Added cursor pointer to buttons, checkboxes, radios.

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,10 @@ export function toggleDisplayNone(element, isDisplayNone) {
 	toggleClassName(element, "display-none", isDisplayNone);
 }
 
+export function toggleBackgroundNone(element, isBackgroundNone) {
+	toggleClassName(element, "background-none", isBackgroundNone);
+}
+
 function toggleClassName(element, className, enabled) {
 	if (enabled) {
 		element.classList.add(className);
